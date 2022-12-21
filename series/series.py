7507468fb7f181248c6b400ea19fffae6e145962
Nerissa Leynes <leynes.nerissa@gmail.com>
2022-12-20 21:11:31 -0500

    updated README template, added function for fibonacci, lucas, and tests run

--- series/series.py
+++ series/series.py
@@ -1,2 +1,24 @@
 def fibonacci(n):
-    pass
+
+    # checks if n <= 0, then return 0
+    if n == 0:
+        return 0
+    # checks if n == 1, then return 1
+    elif n == 1 or n == 2:
+        return 1
+    # checks if n < 0
+    elif n < 0:
+        return "Please enter numbers >= 0"
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+def lucas(n):
+    #chceks if n <= 0, then return 2
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    elif n < 0:
+        return "Please enter numbers >= 0"
+    else:
+        return lucas(n - 1) + lucas(n - 2)
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,6 +1,75 @@
 import pytest
 # from package_name.module_name import function_name
 from series.series import fibonacci
+from series.series import lucas
 
-def fibonacci(n):
-    pass
+# pytest tests must start with "test_"
+
+
+"""
+Below is the test run for function lucas()
+"""
+def test_fibonacci_exists():
+    assert fibonacci
+
+
+def test_fibonacci_zero():
+    actual = fibonacci(0)
+    expected = 0
+    assert actual == expected
+
+
+def test_fibonacci_one():
+    actual = fibonacci(1)
+    expected = 1
+    assert actual == expected
+
+
+def test_fibonacci_other():
+    actual = fibonacci(-2)
+    expected = "Please enter numbers >= 0"
+    assert actual == expected
+
+
+def test_fibonacci_two():
+    actual = fibonacci(2)
+    expected = 1
+    assert actual == expected
+
+def test_fibonacci_eight():
+    actual = fibonacci(8)
+    expected = 21
+    assert actual == expected
+
+"""
+Below is the test run for function lucas()
+"""
+
+def test_lucas_exists():
+    assert lucas
+
+
+def test_lucas_zero():
+    actual = lucas(0)
+    expected = 2
+    assert actual == expected
+
+
+def test_lucas_one():
+    actual = lucas(1)
+    expected = 1
+    assert actual == expected
+
+
+def test_lucas_two():
+    actual = lucas(3)
+    expected = 4
+    assert actual == expected
+
+
+def test_lucas_seven():
+    actual = lucas(7)
+    expected = 29
+    assert actual == expected
+
+
