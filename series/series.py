c163cd584e60f8911311b158ff901e42d694bbef
Nerissa Leynes <leynes.nerissa@gmail.com>
2022-12-20 23:47:15 -0500

    created sum_series(), added tests run

--- series/series.py
+++ series/series.py
@@ -1,5 +1,6 @@
 def fibonacci(n):
-
+    """ This function return the nth value in
+    the fibonacci series"""
     # checks if n <= 0, then return 0
     if n == 0:
         return 0
@@ -13,7 +14,9 @@ def fibonacci(n):
         return fibonacci(n - 1) + fibonacci(n - 2)
 
 def lucas(n):
-    #chceks if n <= 0, then return 2
+    """This function return the nth value in
+        the lucas series"""
+    #checks if n <= 0, then return 2
     if n == 0:
         return 2
     elif n == 1:
@@ -22,3 +25,18 @@ def lucas(n):
         return "Please enter numbers >= 0"
     else:
         return lucas(n - 1) + lucas(n - 2)
+
+
+def sum_series(req, opt1, opt2):
+    if opt1 == 0 and opt2 == 1:
+        return fibonacci(req)
+    elif opt1 == 2 and opt2 == 1:
+        return lucas(req)
+    else:
+        if req == 0:
+            return opt1
+        elif req == 1:
+            return opt2
+        else:
+            return sum_series(req - 1, opt1, opt2) + sum_series(req - 2, opt1, opt2)
+
--- tests/test_series.py
+++ tests/test_series.py
@@ -2,12 +2,13 @@ import pytest
 # from package_name.module_name import function_name
 from series.series import fibonacci
 from series.series import lucas
+from series.series import sum_series
 
 # pytest tests must start with "test_"
 
 
 """
-Below is the test run for function lucas()
+Below is the test run for function fibonacci()
 """
 def test_fibonacci_exists():
     assert fibonacci
@@ -70,6 +71,37 @@ def test_lucas_two():
 def test_lucas_seven():
     actual = lucas(7)
     expected = 29
+    assert actual == expected
+
+
+"""
+Below is the test run for function sum_series()
+"""
+
+def test_sum_series_seven():
+    # fibonacci sequence
+    actual = sum_series(7, 0, 1)
+    expected = 13
+    assert actual == expected
+
+
+def test_sum_series_eight():
+    # lucas sequence
+    actual = sum_series(8, 2, 1)
+    expected = 47
+    assert actual == expected
+
+def test_sum_series_ten():
+    # fibonacci sequence
+    actual = sum_series(10, 0, 1)
+    expected = 55
+    assert actual == expected
+
+
+def test_sum_series_ten():
+    # lucas sequence
+    actual = sum_series(10, 2, 1)
+    expected = 123
     assert actual == expected
 
 
